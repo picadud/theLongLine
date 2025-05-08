155b9b0c8b1d3480801027a8368e33a4127155eb
wuy26 <dunnolatre@gmail.com>
2025-05-08 10:46:04 +0300

    definition of the long ray

--- TheLongLine/Basic.lean
+++ TheLongLine/Basic.lean
@@ -1,1 +1,7 @@
-def hello := "world"
+
+import Mathlib.SetTheory.Cardinal.Aleph
+import Mathlib.Topology.Algebra.Module.ModuleTopology
+import Mathlib.Tactic
+open Topology Set Ordinal
+#check ω_ 1
+def L:=   Set ((ω_ 1).toType × (Ico (0:ℝ)  1))
