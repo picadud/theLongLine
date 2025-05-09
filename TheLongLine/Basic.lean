7aa559ec766282c8b3040e796db2b2acf10c3651
wuy26 <dunnolatre@gmail.com>
2025-05-09 17:36:55 +0300

    laid out structure for the lemma not_second_countable_if_uncountable_discrete_subset

--- TheLongLine/Basic.lean
+++ TheLongLine/Basic.lean
@@ -2,6 +2,50 @@
 import Mathlib.SetTheory.Cardinal.Aleph
 import Mathlib.Topology.Algebra.Module.ModuleTopology
 import Mathlib.Tactic
-open Topology Set Ordinal
-#check ω_ 1
-def L:=   Set ((ω_ 1).toType × (Ico (0:ℝ)  1))
+import Mathlib.Data.Real.Basic
+import Mathlib.Order.Basic
+
+
+open Topology TopologicalSpace Set Ordinal
+#check Ordinal.toType ω₁
+#check ω₁
+
+
+def Ico01 := (Ico (0:ℝ)  1)
+def L:=   (ω_ 1).toType ×ₗ Ico01
+
+noncomputable example : PartialOrder (ω_ 1).toType := by infer_instance
+noncomputable example : PartialOrder Ico01 := by infer_instance
+noncomputable example : PartialOrder ((ω_ 1).toType ×ₗ Ico01) := by infer_instance
+#synth PartialOrder ((ω_ 1).toType ×ₗ Ico01)
+noncomputable instance : PartialOrder L := Prod.Lex.instPartialOrder (ω_ 1).toType ↑Ico01
+instance : TopologicalSpace L := Preorder.topology L
+
+#check Uncountable
+#check exists_countable_basis
+#check singletons_open_iff_discrete
+lemma not_second_countable_if_uncountable_discrete_subset (X: Type) [hX:TopologicalSpace X]
+    (S: Set X) (h: ¬ S.Countable) (h1: DiscreteTopology S): ¬ SecondCountableTopology X:= by
+  by_contra hX
+
+  apply exists_countable_basis at hX
+  cases' hX with B hB
+  cases' hB with hBc hB
+  cases' hB with hB1 hB2?
+  rw[ ← singletons_open_iff_discrete ] at h1
+  have hS: Nonempty S := by
+    refine nonempty_iff_ne_empty'.mpr ?_
+    by_contra hse
+    have he: (∅: Set X).Countable := by exact Subsingleton.to_countable
+    rw[← hse] at he
+    exact h he
+
+
+
+
+theorem firstCountable_LongLine: FirstCountableTopology L:= by
+
+  sorry
+#check isSecondCountable
+theorem not_secondCountable_LongLine: ¬ SecondCountableTopology L:= by
+  sorry
